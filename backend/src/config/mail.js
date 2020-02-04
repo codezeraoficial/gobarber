cf4cdefff9636e72f26dbcd11c16175983661fad
Leonardo Viana <leonardo.viana@meep-app.com>
2020-02-04 19:41:27 -0300

    #9

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'fbde3ca4f4026d',
+    pass: '9be6a528827edc',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
